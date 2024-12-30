0d9ed8e97bfa7c0aa4dc7d6b04f3ef0acb72037b
Martarion-Code <mharionmarterl@gmail.com>
2024-12-31 06:47:43 +0700

    change in admin ps

--- source-code/src/app/api/ps/create/route.js
+++ source-code/src/app/api/ps/create/route.js
@@ -6,14 +6,14 @@ import db from '@/lib/db';
 export async function POST(request) {
   try {
     const body = await request.json();
-    const { kategori, harga, stok } = body;
+    const { kategori, seri, harga, stok } = body;
 
     if (!kategori || !harga || typeof stok !== 'number') {
       return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
     }
 
     const newPs = await db.ps.create({
-      data: { kategori, harga, stok },
+      data: { kategori, seri, harga, stok },
     });
 
     return NextResponse.json(newPs, { status: 201 });
